0a3fd746dba177c1879983931a145ad1b0714041
manugarg <manugarg@1b6c34b2-4227-0410-ae3c-bf0be58fe8b8>
2005-06-29 15:30:59 +0000

    Fixed an issue with the executable on windows. Now handles both cases -- running as a script and an executable properly.
    Running as a script, sets up a link to pymailerw.py, while running as executable sets up a link to executable pymailerw
    
    
    git-svn-id: https://pymailer.svn.sourceforge.net/svnroot/pymailer/trunk@7 1b6c34b2-4227-0410-ae3c-bf0be58fe8b8

--- installsendto.py
+++ installsendto.py
@@ -1,9 +1,14 @@
-import sys, os
+import sys, os, os.path
 
 userhome = os.environ['USERPROFILE']
-pypath=sys.prefix+'\\python'
 cwd=os.getcwd()
 
 file=open(userhome+'\\SendTo\\pyMailer.bat','w')
-file.write(pypath+' '+cwd+'\\pymailerw.py %*')
+fname, ext = os.path.splitext(sys.argv[0])
+
+if ext == ".py":
+	pypath=sys.prefix+'\\python'
+	file.write(pypath+' '+cwd+'\\pymailerw.py %*')
+else:
+	file.write(cwd+'\\pymailerw %*')	
 file.close()
