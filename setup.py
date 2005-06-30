c52a2be0b0714c680b28a7b32536bf44f37755bd
manugarg <manugarg@1b6c34b2-4227-0410-ae3c-bf0be58fe8b8>
2005-06-30 05:44:45 +0000

    Fixed encodings issue.
    git-svn-id: https://pymailer.svn.sourceforge.net/svnroot/pymailer/trunk@8 1b6c34b2-4227-0410-ae3c-bf0be58fe8b8

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 import py2exe
-      
-setup(console=["pymailer.py"])
-setup(windows=["pymailerw.py"])
+
+setup(console=["pymailer.py"], 
+		options = {"py2exe": {"packages": ["encodings"]}},
+		)
+setup(windows=["pymailerw.py"], 
+		options = {"py2exe": {"packages": ["encodings"]}},
+		)
 setup(console=["installsendto.py"])
